76e744eb2ec0455202eb8d6aa8cae4e6d0317b12
Anna Bozio <anna@tableonline.fr>
2023-10-13 00:03:39 +0200

    create mission model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_12_212508) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_12_220147) do
   create_table "games", force: :cascade do |t|
     t.string "status", null: false
     t.string "game_type", null: false
@@ -19,6 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_212508) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
+  create_table "missions", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "players", force: :cascade do |t|
